c95f4161553fa3d8a0c8d13e87f6adafe8449c9d
Arch1js <a.dobrajs@gmail.com>
2016-06-13 15:46:42 +0100

    make it better

---
+++ public-final/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://Archijs:artu3005@ds013584.mlab.com:13584/game_location_db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
