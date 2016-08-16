9bd65cb295746f4b5a5be927f54fbde19dca3217
Arch1js <a.dobrajs@gmail.com>
2016-08-16 18:55:39 +0100

    Cosmetic changes in user profiles (when viewed from other accounts)

--- views/js/directive.js
+++ views/js/directive.js
@@ -1,6 +1,13 @@
 app.directive("navbar", function () { //navigation bar
 	return {
 		restrict: "A",
-		templateUrl: "/../navbar.html"
+		templateUrl: "/../templates/navbar.html"
+	}
+});
+
+app.directive("footer", function () { //navigation bar
+	return {
+		restrict: "A",
+		templateUrl: "/../templates/footer.html"
 	}
 });
