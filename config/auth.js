1b7dbecb416cf804a7ce7e743b9a0c6d00fda5d4
Arch1js <a.dobrajs@gmail.com>
2016-09-24 14:02:30 +0100

    Changed facebook authentication callback to heroku

--- config/auth.js
+++ config/auth.js
@@ -3,13 +3,6 @@
 // expose our config directly to our application using module.exports
 module.exports = {
 
-	// 'facebookAuth' : {
-	// 	'clientID' 		: '1610783645902344', // your App ID
-	// 	'clientSecret' 	: '9fc07c34614c62d970b37a30bfc0cb78', // your App Secret
-	// 	'callbackURL' 	: 'https://localgame.herokuapp.com/auth/facebook/callback'
-	// 	// 'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
-	// },
-
 	'facebookAuth' : {
 		'clientID' 		: '894368400690915', // your App ID
 		'clientSecret' 	: '92daa134f5535967308610ef0add20a7', // your App Secret
